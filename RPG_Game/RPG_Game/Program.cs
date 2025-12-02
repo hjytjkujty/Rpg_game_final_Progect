88800b3b8948e808dccdb56f4e0d0e2d262b91af
mark <markiyan28102009@gmail.com>
2025-12-02 13:03:32 +0200

    add character

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -1,5 +1,20 @@
 ﻿namespace RPG_Game
 {
+    public interface IEquippabe
+    {
+        string Name { get; }
+        void Equip();
+        void Enequip();
+    }
+
+    public abstract class Character
+    {
+        private string _name;
+        private int _health;
+        private int _maxHealth;
+        private int _strenght;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
