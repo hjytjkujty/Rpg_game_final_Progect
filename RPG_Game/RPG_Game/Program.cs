a23147c43b76a2980dc74af9693a97b7d5337942
mark <markiyan28102009@gmail.com>
2025-12-09 12:34:30 +0200

    add class Item

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -7,6 +7,25 @@
         void Enequip();
     }
 
+    public class Player
+    {
+
+    }
+
+    public abstract class Item
+    {
+        public string Name { get; protected set; }
+        public string Desscription { get; protected set; }
+
+        protected Item(string name, string desscription)
+        {
+            Name = name;
+            Desscription = desscription;
+        }
+
+        public abstract void Use(Player player);
+    }
+
     internal class Program
     {
         static void Main(string[] args)
