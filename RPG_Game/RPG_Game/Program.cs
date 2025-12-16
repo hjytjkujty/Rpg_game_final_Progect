995c59d1cf81867e63bad8ec85eed6ac414239b7
mark <markiyan28102009@gmail.com>
2025-12-16 13:05:10 +0200

    add Goblin

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -22,6 +22,21 @@
         }
     }
 
+    public class Goblin : Enemy
+    {
+        public Goblin() : base("гоблін", 40, 8, 30)
+        {
+            AddLoot(new HealthPoition());
+        }
+
+        public override void Attack(Character target)
+        {
+            var damage = Strenght;
+            Console.WriteLine($"{Name} б'є кінжалом і завдає {damage}");
+            target.TakeDamage(damage);
+        }
+    }
+
     public class Player : Character, ISpellCast
     {
 
